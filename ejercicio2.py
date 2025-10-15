e8960b223bb64dfeffbad5885f87ac242d7038e4
unknown <helenapcarrera@gmail.com>
2025-10-15 17:23:14 +0200

    ejercicios

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,8 +1,13 @@
 """Enunciado: declaramos variables con nombres descriptivos, calculamos el precio final con IVA y formateamos una cadena de salida."""
+from decimal import Decimal, getcontext
 
-IVA = 21
-precio_base = 120.0
+getcontext().prec = 10
 
-calculadora_Iva = (precio_base * IVA)/100
+IVA = Decimal("21")
+precio_base = Decimal("120")
+
+calculadora_Iva = (precio_base * IVA)/Decimal("100")
 precio_final = calculadora_Iva + precio_base;
-print(f"El IVA del producto es de {calculadora_Iva} euros. Precio final {precio_final}")
+
+#Lo de 5f significa que quiero que muestre 5 decimales
+print(f"El IVA del producto es de {calculadora_Iva:5f} euros. Precio final {precio_final:5f}")
--- ejercicio3.py
+++ ejercicio3.py
@@ -1,7 +1,29 @@
 """normalizamos un nombre propio y extraemos iniciales."""
 
-nombre_completo = "Helena Peño Carrera"
+nombre_completo = " Ana MARÍA López "
+partes_normalizadas =  []
 
-print(nombre_completo[0])
-print(nombre_completo[7])
-print(nombre_completo[12])
+#Elimina los huecos de delante y detrás
+nombre_sin_espacios = nombre_completo.strip()
+
+#Divide en partes
+partes = nombre_sin_espacios.split()
+
+for palabra in partes: 
+    #Primero se pone todas las palabras en minúsculas y después solo la primera letra de cada palabra
+    palabra_normalizada = palabra.lower().capitalize()
+    partes_normalizadas.append(palabra_normalizada)
+
+normalizado = " ".join(partes_normalizadas)
+
+iniciales_lista = []
+for palabra in partes_normalizadas:
+    inicial = palabra[0]
+    iniciales_lista.append(inicial)
+
+iniciales = ".".join(iniciales_lista)
+
+assert normalizado == "Ana María López"
+assert iniciales == "A.M.L"
+
+print("OK: ej2")
