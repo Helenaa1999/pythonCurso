837528a5e1f1c4515d7176a05bac441f93c85964
unknown <helenapcarrera@gmail.com>
2025-10-17 13:43:14 +0200

    probando ejercicio clientes y factura

--- ejercicioClientesFacturas.py
+++ ejercicioClientesFacturas.py
@@ -63,7 +63,6 @@ class Cliente(Persona):
     def email(self, email):
         self.email.setter = email
 
-
     def __del__(self):
         print(f"Ciente id: {self.id_cliente} eliminado.")
 
@@ -83,5 +82,51 @@ class Factura():
 
     def __eq__(self, other):
         if isinstance(other, Factura):
-            return self
+            return self.id_factura == other.id_factura and self.cliente == other.cliente
+        return False
+    
+    @property
+    def id_factura(self):
+        return self.id_factura
+
+    @id_factura.setter
+    def id_factura(self, id_factura):
+        self.id_factura = id_factura
+
+    @property
+    def cliente(self):
+        return self.cliente
+    
+    
+
+if __name__ == "__main__":
+    mi_cliente = Cliente("4332542F", "Juan", "Pérez",
+                         12342, "juan.perez@example.com")
+    instancia1 = Cliente("4333242F", "Pedro", "Pérez",
+                         12343, "jpedro@example.com")
+    
+    Cliente.mostrar_numero_de_instancias()
+    instancia2 = Cliente("43397442F", "Pepe", "Pérez",
+                         1249783, "jpepe@example.com")
+    Cliente.mostrar_numero_de_instancias()
+    instancia3 = Cliente("4343242F", "Carlos", "Pérez",
+                         12453, "jcarlos@example.com")
+    
+    instancia3.id_cliente = 443
+    Cliente.mostrar_numero_de_instancia()
+
+    factura1 = Factura("FAC001", mi_cliente)
+    factura2 = Factura("FAC001", mi_cliente)
+
+    if(factura1 == factura2):
+        print("Las facturas son las mismas.")
+    else:
+        print("Las facturas son distintas")
+
+    factura1.mostrar_detalle()
+    print(factura1)
+
+    miset = {factura1, factura2}
+    print(miset)
+
     
